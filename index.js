39e95f612eae987a4a9fe3f238a8b338893c5bc0
Vitalii Hromyk <hromykk@gmail.com>
2021-11-30 22:59:50 +0200

    Initializated commit

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 require('dotenv').config()
-// const { Telegraf } = require('telegraf')
 const { Composer } = require('micro-bot')
 const axios = require('axios')
 
-// const bot = new Telegraf(process.env.BOT_TOKEN)
 const bot = new Composer()
 
 bot.start(ctx => {
